b5c2edc581bdadc21684c948bc5b0c9acfdcafd7
shoalwave <shoalwave.dev@gmail.com>
2022-06-15 17:54:28 +0900

    function->rcm, rcoを追加

--- config.fish
+++ config.fish
@@ -5,7 +5,6 @@ source ~/.config/fish/config_functions.fish
 
 #! alias
 alias vi='nvim'
-alias update='echo -e "\n\e[33m##### Update list of packages. #####\e[m\n" && sudo apt update && echo -e "\n\e[33m##### List of packages is updated. #####\n##### NEXT: Upgrade all packages #####\e[m\n" && sudo apt upgrade && echo -e "\n\e[33m##### All installed packages are upgraded. #####\n##### NEXT: Remove packages that are no longer needed #####\e[m\n" && sudo apt autoremove && echo -e "\n\e[33m##### Packages that are no longer needed for the update are removed. #####\n##### NEXT: Clean up cached files #####\e[m\n" && sudo apt clean && echo -e "\n\e[33m##### all cached deb files are deleted. #####\e[m\n"'
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
@@ -18,6 +17,7 @@ else
     #! for other OS(Linux).
     eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source
     set -xg PATH $HOME/.npm-global/bin $PATH
+    alias update='echo -e "\n\e[33m##### Update list of packages. #####\e[m\n" && sudo apt update && echo -e "\n\e[33m##### List of packages is updated. #####\n##### NEXT: Upgrade all packages #####\e[m\n" && sudo apt upgrade && echo -e "\n\e[33m##### All installed packages are upgraded. #####\n##### NEXT: Remove packages that are no longer needed #####\e[m\n" && sudo apt autoremove && echo -e "\n\e[33m##### Packages that are no longer needed for the update are removed. #####\n##### NEXT: Clean up cached files #####\e[m\n" && sudo apt clean && echo -e "\n\e[33m##### all cached deb files are deleted. #####\e[m\n"'
 end
 
 #! initialize Python
